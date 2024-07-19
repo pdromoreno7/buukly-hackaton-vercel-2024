b20cbc1167eba81bba72b08da91d2f07111f5f9e
Abel Guardo <abelguardo.dev@gmail.com>
2024-07-18 19:09:34 -0500

    add basic settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -8,13 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'dark': '#18181b'
+      }
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
