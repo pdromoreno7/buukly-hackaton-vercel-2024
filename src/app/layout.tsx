f5951fd3e3632b02885993e762bbebe11bd27043
pdromoreno7 <pdromorenodev@gmail.com>
2024-07-26 15:30:03 -0500

    change: light to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='es' className='light'>
+    <html lang='es' className='dark'>
       <body className={onest.className}>
         <Header />
         <main>{children}</main>
