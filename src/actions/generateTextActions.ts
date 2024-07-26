d55837dab300253fc704939f1ce578191af91a7f
pdromoreno7 <pdromorenodev@gmail.com>
2024-07-25 23:56:06 -0500

    feat: zond installed and shema zod data structuring for ebook data

--- src/actions/generateTextActions.ts
+++ src/actions/generateTextActions.ts
@@ -1,23 +1,34 @@
 'use server'
 import { google } from '@ai-sdk/google'
-import { generateText } from 'ai'
-
-export async function runGeminiText(query: string): Promise<string> {
-  try {
-    const { text } = await generateText({
-      model: google('models/gemini-pro'),
-      prompt: `Eres un escritor profesional de libros electrónicos con 20 años de experiencia. Tu estilo es claro, conciso y mantiene un tono formal.
+import { generateObject } from 'ai'
+import { z } from 'zod'
+export async function runGeminiObject(query: string): Promise<{
+  recipe: { bookTitle: string; bookChapters: string[]; bookKeyWords: string[] }
+}> {
+  const PROMPT_GENERATE_TITLE_SHAPTERS_BOOK = `Eres un escritor profesional de libros electrónicos con 20 años de experiencia. Tu estilo es claro, conciso y mantiene un tono formal.
 Redactas títulos de libros electrónicos increíblemente convincentes: 
 
 Genera un título altamente convincente para un libro electrónico sobre el tema de ${query}. Responde solo con el título del libro electrónico. No pongas comillas (*) alrededor del título del libro electrónico
 Luego, Redacta una lista de 10 capítulos para el libro electrónico titulado como el titulo que creaste y que va a tratar sobre ${query} Proporciona solo los títulos de los capítulos en forma de viñetas. No escribas subcapítulos ni esquemas de los capítulos. Responde solo con una lista de títulos de capítulos.
 Enumera palabras clave para un libro electrónico sobre el tema ${query} titulado como lo que creaste y con la lista de capítulos que creaste.
-Crea un json que tenga la siguiete estructura: bookTitle: "", bookChapters: [], bookKeyWords:[] del libro. en bookTitle sera un string donde pondras el nombre del titulo del libro que creaste, en bookChapters sera una lista (array) de capítulos que craestes, solo pone el nombre del capítulo, y en bookKeyWords tambien sera na lista de palabras basdo en bookTitle y bookChapters que creastes. Solo responde con el JSON.`,
+Crea un json que tenga la siguiete estructura: bookTitle: "", bookChapters: [], bookKeyWords:[] del libro. en bookTitle sera un string donde pondras el nombre del titulo del libro que creaste, en bookChapters sera una lista (array) de capítulos que craestes, solo pone el nombre del capítulo, y en bookKeyWords tambien sera na lista de palabras basdo en bookTitle y bookChapters que creastes. Solo responde con el JSON.`
+
+  try {
+    const { object } = await generateObject({
+      model: google('models/gemini-1.5-pro'),
+      schema: z.object({
+        recipe: z.object({
+          bookTitle: z.string(),
+          bookChapters: z.array(z.string()),
+          bookKeyWords: z.array(z.string()),
+        }),
+      }),
+      prompt: PROMPT_GENERATE_TITLE_SHAPTERS_BOOK,
     })
 
-    return text
+    return object
   } catch (error) {
-    console.error('Error generating text:', error)
-    throw new Error('Failed to generate text')
+    console.error('Error generando texto:', error)
+    throw new Error('No se pudo generar el texto')
   }
 }
--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,32 +1,12 @@
 'use client'
 // import { GoogleGenerativeAI } from '@google/generative-ai'
-import { runGeminiText } from '@/actions/generateTextActions'
+import { runGeminiObject } from '@/actions/generateTextActions'
 import { useState, ChangeEvent } from 'react'
 
 import Wrapper from '@/components/layouts/Wrapper'
 import Steps from '@/components/steps/Steps'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-
-// const API_KEY = process.env.NEXT_PUBLIC_API_GEMINI_KEY
-// if (!API_KEY) throw new Error('Missing API_KEY')
-// const genAI = new GoogleGenerativeAI(API_KEY)
-// const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro' })
-// async function runGemini(title: string) {
-//   const prompt = `Eres un escritor profesional de libros electrónicos con 20 años de experiencia. Tu estilo es claro, conciso y mantiene un tono formal.
-// Redactas títulos de libros electrónicos increíblemente convincentes:
-
-// Genera un título altamente convincente para un libro electrónico sobre el tema de ${title}. Responde solo con el título del libro electrónico. No pongas comillas (*) alrededor del título del libro electrónico
-// Luego, Redacta una lista de 10 capítulos para el libro electrónico titulado como el titulo que creaste y que va a tratar sobre ${title} Proporciona solo los títulos de los capítulos en forma de viñetas. No escribas subcapítulos ni esquemas de los capítulos. Responde solo con una lista de títulos de capítulos.
-// Enumera palabras clave para un libro electrónico sobre el tema ${title} titulado como lo que creaste y con la lista de capítulos que creaste.
-// Crea un json que tenga la siguiete estructura: bookTitle: "", bookChapters: [], bookKeyWords:[] del libro. en bookTitle sera un string donde pondras el nombre del titulo del libro que creaste, en bookChapters sera una lista (array) de capítulos que craestes, solo pone el nombre del capítulo, y en bookKeyWords tambien sera na lista de palabras basdo en bookTitle y bookChapters que creastes. Solo responde con el JSON.
-// `
-
-//   const result = await model.generateContent(prompt)
-//   const response = await result.response
-//   const text = response.text()
-//   return text
-// }
 
 export default function Generate() {
   const [bookTitle, setBookTitle] = useState<string>('')
@@ -36,7 +16,7 @@ export default function Generate() {
   }
 
   const submitGenerateBook = async () => {
-    const result = await runGeminiText(bookTitle)
+    const result = await runGeminiObject(bookTitle)
     console.log(result)
   }
 
