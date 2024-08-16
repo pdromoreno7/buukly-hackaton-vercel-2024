284dafe1af279a56d1b4560fba8ce3de57741b4a
pdromoreno7 <pdromorenodev@gmail.com>
2024-08-16 11:05:49 -0500

    fix: page  read book

--- src/app/read-ebook/page.tsx
+++ src/app/read-ebook/page.tsx
@@ -66,3 +66,14 @@
 //     </div>
 //   )
 // }
+'use client'
+
+export default function BookView() {
+  return (
+    <div className='flex h-full flex-col'>
+      <main className='container mx-auto max-w-3xl px-4 py-0 lg:px-0'>
+        <h1>Read a book</h1>
+      </main>
+    </div>
+  )
+}
